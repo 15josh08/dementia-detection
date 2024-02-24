cf090089ef847832346b9d42d0216419d6797a9c
15josh08 <joshrajendrapatel@gmail.com>
2024-02-23 19:42:15 -0500

    uploaded files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+import os
 from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
@@ -36,4 +37,5 @@ def predict():
     return render_template('result.html', prediction=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=True, host = "0.0.0.0", port = port)
